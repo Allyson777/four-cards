3e0fe43fbe05af51aabff53c517a4b61fb5ade06
Allyson Pina <allyson.viana@edu.ufes.br>
2025-02-03 23:45:56 -0300

    add config fonts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,16 @@
 import "@/styles/globals.css";
+import { Poppins } from "next/font/google"
 import type { AppProps } from "next/app";
 
+const poppins = Poppins({
+  weight: ["200", "400", "600"],
+});
+
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <main className={poppins.className}>
+      <Component {...pageProps} />
+    </main>
+  )
 }
